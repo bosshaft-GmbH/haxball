d640d16412ca76c319bec2e922d63758e70f6400
sublime801 <55764426+sublime801@users.noreply.github.com>
2019-09-28 15:25:17 +0300

    Way better version with noPlayer option.

--- examples/avatar/avatar.js
+++ examples/avatar/avatar.js
@@ -1,33 +1,46 @@
 var room = HBInit({
-        roomName: "https://github.com/bosshaft-GmbH",
-        maxPlayers: 6,
-        playerName: "Bosshaft GmbH",
-        public: !1,
-        geo: {
-            code: "TR",
-            lat: 41.00824,
-            lon: 28.978359
-        }
-    }),
+    roomName: "https://github.com/bosshaft-GmbH",
+    maxPlayers: 16,
+    public: !1,
+    noPlayer: !0,
+    geo: {
+        code: "TR",
+        lat: 41.00824,
+        lon: 28.978359
+    }
+}),
     commands = {
-        "!avatar": bosshaftAvatar
+        "!customball": bosshaftColor,
+        "!ball" : bosshaftColorString
+    },
+    colors = {
+        "red": 16711680,
+        "green": 65280,
+        "black": 0,
+        "transparent": -1
     };
 
-function bosshaftAvatar(t, o) {
-    let a = o.substr(8),
-        r = a.split("-").slice(0),
-        e = a.split("-").slice(1);
-    return room.setPlayerAvatar(r[0], e[0]), !1
+function bosshaftColor (o, r) {
+    let e = r.split(/ +/);
+    return room.setDiscProperties(0, {
+        color: e[0]
+    }), !1
 }
 
-function updateAdmins() {
-    var t = room.getPlayerList().filter(t => 0 != t.id);
-    0 != t.length && null == t.find(t => t.admin) && room.setPlayerAdmin(t[0].id, !0)
+function bosshaftColorString (o, r) {
+    let e = r.split(/ +/);
+    return (colors.hasOwnProperty(e[0].toLowerCase()) ? room.setDiscProperties(0, {
+        color: colors[e[0].toLowerCase()]
+    }) : room.sendAnnouncement("That color is not defined !", o.id)), !1
 }
-room.setDefaultStadium("Classic"), room.setScoreLimit(3), room.setTimeLimit(0), room.onPlayerJoin = function(t) {
+
+function updateAdmins () {
+    var o = room.getPlayerList();
+    0 != o.length && null == o.find(o => o.admin) && room.setPlayerAdmin(o[0].id, !0)
+}
+room.setDefaultStadium("Classic"), room.setScoreLimit(3), room.setTimeLimit(0), room.onPlayerJoin = function (o) {
     updateAdmins()
-}, room.onPlayerChat = function(t, o) {
-    let a = o.search(" "),
-        r = o.substr(0, -1 !== a ? a : o.length);
-    if (1 == commands.hasOwnProperty(r)) return commands[r](t, o)
+}, room.onPlayerChat = function (o, r) {
+    let e = r.split(/ +/);
+    if (1 == commands.hasOwnProperty(e[0].toLowerCase())) return commands[e[0].toLowerCase()](o, r)
 };
